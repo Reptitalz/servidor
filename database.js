0524f7fef238bb0d78cf899b37f3a9da5bb7d7f1
ReptiTalz <106882226+Reptitalz@users.noreply.github.com>
2024-03-16 19:44:43 +0000

    actualizacion marzo 16 - 2

--- database.js
+++ database.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 
 (async () => {
-  const db = await mongoose.connect('mongodb://localhost/reptitalz');
+  const db = await mongoose.connect("mongodb://Localhost/reptitalz", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   console.log('Conexión exitosa a la base de datos ' + db.connection.name);
 })();
